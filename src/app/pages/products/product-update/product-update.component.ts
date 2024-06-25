f4085dec880e2af02274ac8ce3eba50440f56602
natalia-coelho <nataliacoelho199@gmail.com>
2024-06-24 22:00:12 -0300

    Implements products updte component

--- src/app/pages/products/product-update/product-update.component.ts
+++ src/app/pages/products/product-update/product-update.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { Produto } from 'src/app/models/Produto';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
   styleUrls: ['./product-update.component.scss']
 })
-export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+export class ProductUpdateComponent implements OnInit {
+  product: any;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    const productId = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProductById(productId).subscribe(
+      (product: Produto) => {
+        this.product = product;
+        console.log('Product Details:', this.product);
+      },
+      error => {
+        console.error('Error fetching product:', error);
+      }
+    );
   }
 
+  atualizarProduto(): void {
+    this.router.navigate(['/product-update', this.product.produtoId]);
+  };
+
+  removerProduto(): void {
+    if (confirm('Tem certeza que deseja remover este produto?')) {
+      this.productService.deleteProduct(this.product.id).subscribe(() => {
+        alert('Produto removido com sucesso!');
+        (<any>this.router).navigate(['/produtos']);
+      });
+    }
+  }
 }
