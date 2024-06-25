5c1767897d583b4340aa2e42e3bbccfc3fcf6f29
natalia-coelho <nataliacoelho199@gmail.com>
2024-06-24 21:59:15 -0300

    Add forms to the product module and removing unused components

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -2,27 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
-// import { ProductListComponent } from '../../modules/products/product-list/product-list.component';
-// import { ProductDetailsComponent } from '../../modules/products/product-details/product-details.component';
-// import { ProductFormComponent } from '../../modules/products/product-form/product-form.component';
 import { ProductsComponent } from './products.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductUpdateComponent } from './product-update/product-update.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    // ProductListComponent,
     ProductsComponent,
     ProductDetailsComponent,
     ProductUpdateComponent,
-    // ProductFormComponent
   ],
   imports: [
     CommonModule,
     ComponentsModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    FormsModule
   ]
 })
 
